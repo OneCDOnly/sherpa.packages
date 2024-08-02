f05e0a70763824c996c05b0452822f7704e0e1db
OneCDOnly <one.cd.only@gmail.com>
2024-08-03 06:00:50 +1000

    [update] QPKG archives

--- QPKGs/OTautulli/shared/otautulli.sh
+++ QPKGs/OTautulli/shared/otautulli.sh
@@ -21,7 +21,7 @@
 #*	 You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/
 readonly USER_ARGS_RAW=$*
 readonly QPKG_NAME=OTautulli
-readonly SERVICE_SCRIPT_VERSION='240802'
+readonly SERVICE_SCRIPT_VERSION='240803'
 InitService()
 {
 pip_cache_path=$QPKG_PATH/pip-cache
