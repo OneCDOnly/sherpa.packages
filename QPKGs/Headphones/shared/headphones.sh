3715566d18c5025706d78b62abb9763c4f4d1a89
OneCDOnly <one.cd.only@gmail.com>
2024-08-10 11:50:05 +1000

    [update] archives [pre-merge]

--- QPKGs/Headphones/shared/headphones.sh
+++ QPKGs/Headphones/shared/headphones.sh
@@ -21,7 +21,7 @@
 #*	 You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/
 readonly USER_ARGS_RAW=$*
 readonly QPKG_NAME=Headphones
-readonly SERVICE_SCRIPT_VERSION='240809'
+readonly SERVICE_SCRIPT_VERSION='240810'
 InitService()
 {
 pip_cache_path=$QPKG_PATH/pip-cache
@@ -34,6 +34,7 @@ venv_pip_pathfile=$venv_path/bin/pip
 venv_python_pathfile=$venv_path/bin/python3
 can_restart_to_update=true
 install_pip_deps=true
+recheck_daemon_pid_after_launch=true
 interpreter=/opt/bin/python3
 source_git_branch=master
 source_git_branch_depth=shallow
