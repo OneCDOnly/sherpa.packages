cfca6411b81f708dbf2fb8ebc1d752e7d6e58d9a
OneCDOnly <one.cd.only@gmail.com>
2024-09-14 14:18:25 +1000

    [update] QPKG archives

--- QPKGs/OLidarr/x86_64/olidarr.sh
+++ QPKGs/OLidarr/x86_64/olidarr.sh
@@ -21,7 +21,7 @@
 #*	 You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/
 readonly r_user_args_raw=$*
 readonly r_qpkg_name=OLidarr
-readonly r_service_script_version='240912'
+readonly r_service_script_version='240914'
 InitService()
 {
 qpkg_repo_path=$r_qpkg_path/repo-cache
