27e9b0a5a9ad710a1faaec83cbe0ef7952814354
OneCDOnly <one.cd.only@gmail.com>
2024-09-21 13:51:06 +1000

    [update] QPKG archives

--- QPKGs/ClamAV/shared/clamav.sh
+++ QPKGs/ClamAV/shared/clamav.sh
@@ -36,7 +36,7 @@ StartQPKGCustom()
 {
 IsError && return
 MakePaths
-WaitForGit || { SetError;return 1 ;}
+WatchForGit || { SetError;return 1 ;}
 if [[ ! -e $r_backup_service_pathfile ]];then
 cp "$r_target_service_pathfile" "$r_backup_service_pathfile"
 /bin/sed -i 's|/usr/local/bin/clamscan|/opt/sbin/clamscan|' "$r_target_service_pathfile"
@@ -47,7 +47,7 @@ cp "$r_target_service_pathfile" "$r_backup_service_pathfile"
 eval "$r_target_service_pathfile" restart &>/dev/null
 fi
 /bin/grep -q freshclam /etc/profile || echo "alias freshclam='/opt/sbin/freshclam -u admin --config-file=/etc/config/freshclam.conf --datadir=/share/$(/sbin/getcfg Public path -f /etc/config/smb.conf | cut -d '/' -f 3)/.antivirus/usr/share/clamav -l /tmp/.freshclam.log'" >> /etc/profile
-DisplayCommitToLog 'start: OK'
+DisplayCommitToLog '> start: OK'
 return 0
 }
 StopQPKGCustom()
@@ -58,7 +58,7 @@ mv "$r_backup_service_pathfile" "$r_target_service_pathfile"
 eval "$r_target_service_pathfile" restart &>/dev/null
 fi
 /bin/sed -i '/freshclam/d' /etc/profile
-DisplayCommitToLog 'stop: OK'
+DisplayCommitToLog '> stop: OK'
 return 0
 }
 StatusQPKGCustom()
