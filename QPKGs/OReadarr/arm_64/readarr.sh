7d26d8ba682d59e5f3e45c3f3aeeae8ebc28d141
OneCDOnly <one.cd.only@gmail.com>
2025-05-11 12:49:27 +1000

    [update] QPKG archives

--- QPKGs/OReadarr/arm_64/readarr.sh
+++ QPKGs/OReadarr/arm_64/readarr.sh
@@ -24,7 +24,7 @@
 #*	 You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/
 readonly r_user_args_raw=$*
 readonly r_qpkg_name=OReadarr
-readonly r_service_script_version='250508'
+readonly r_service_script_version='250511'
 InitService(){
 qpkg_repo_path=$r_qpkg_path/repo-cache
 qpkg_ini_file=config.xml
