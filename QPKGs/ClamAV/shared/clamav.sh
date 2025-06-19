c34948d8d5f08477a6675b5d0d83910b5716ae2e
OneCDOnly <one.cd.only@gmail.com>
2025-06-20 08:12:03 +1000

    [update] QPKG archives

--- QPKGs/ClamAV/shared/clamav.sh
+++ QPKGs/ClamAV/shared/clamav.sh
@@ -24,7 +24,7 @@
 #*	 You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/
 readonly r_user_args_raw=$*
 readonly r_qpkg_name=ClamAV
-readonly r_service_script_version='250616'
+readonly r_service_script_version='250620'
 InitService(){
 qpkg_ini_file=undefined
 qpkg_backup_pathfile=undefined
@@ -46,8 +46,8 @@ cp "$r_target_service_pathfile" "$r_backup_service_pathfile"
 /bin/sed -i 's|$FRESHCLAM -u admin -l /tmp/.freshclam.log|$FRESHCLAM -u admin --config-file=$FRESHCLAM_CONFIG --datadir=$ANTIVIRUS_CLAMAV -l /tmp/.freshclam.log|' "$r_target_service_pathfile"
 eval "$r_target_service_pathfile" restart &>/dev/null
 fi
-/bin/grep -q freshclam /etc/profile ||echo "alias freshclam='/opt/sbin/freshclam -u admin --config-file=/etc/config/freshclam.conf --datadir=/share/$(/sbin/getcfg Public path -f /etc/config/smb.conf|cut -d '/' -f 3)/.antivirus/usr/share/clamav -l /tmp/.freshclam.log'">>/etc/profile
-DisplayCommitToLog '> start: OK'
+[[ "$(</etc/profile)" =~ "freshclam" ]]||echo "alias freshclam='/opt/sbin/freshclam -u admin --config-file=/etc/config/freshclam.conf --datadir=/share/$(/sbin/getcfg Public path -f /etc/config/smb.conf|cut -d '/' -f 3)/.antivirus/usr/share/clamav -l /tmp/.freshclam.log'">>/etc/profile
+DisplayAndCommitInfoToLog 'start: OK'
 return 0;}
 StopQPKGCustom(){
 IsError &&return
@@ -56,7 +56,7 @@ mv "$r_backup_service_pathfile" "$r_target_service_pathfile"
 eval "$r_target_service_pathfile" restart &>/dev/null
 fi
 /bin/sed -i '/freshclam/d' /etc/profile
-DisplayCommitToLog '> stop: OK'
+DisplayAndCommitInfoToLog 'stop: OK'
 return 0;}
 StatusQPKGCustom(){
 IsNotError ||return
