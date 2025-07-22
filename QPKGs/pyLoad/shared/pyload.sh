6e53d6bb060faaa7ce7463bedbf81b31dd1eb14a
OneCDOnly <one.cd.only@gmail.com>
2025-07-23 08:40:46 +1000

    [update] QPKG archives

--- QPKGs/pyLoad/shared/pyload.sh
+++ QPKGs/pyLoad/shared/pyload.sh
@@ -37,7 +37,7 @@
 #*
 readonly r_user_args_raw=$*
 readonly r_qpkg_name=pyLoad
-readonly r_service_script_version=250722
+readonly r_service_script_version=250723
 InitService(){
 pip_cache_path=$r_qpkg_path/pip-cache
 qpkg_wheels_path=$r_qpkg_path/qpkg-wheels
