1f26a1bf4243fe246398076cd3cd2aeec96323bc
OneCDOnly <one.cd.only@gmail.com>
2025-07-25 07:50:56 +1000

    [update] QPKG archives

--- QPKGs/OLidarr/arm_64/lidarr.sh
+++ QPKGs/OLidarr/arm_64/lidarr.sh
@@ -37,7 +37,7 @@
 #*
 readonly r_user_args_raw=$*
 readonly r_qpkg_name=OLidarr
-readonly r_service_script_version=250724
+readonly r_service_script_version=250725
 InitService(){
 qpkg_repo_path=$r_qpkg_path/repo-cache
 qpkg_ini_file=config.xml
