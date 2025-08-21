b2e9c85449fb0052e48010d296dabb148db1016d
OneCDOnly <one.cd.only@gmail.com>
2025-08-22 08:24:34 +1000

    [update] QPKG archives

--- QPKGs/ClamAV/shared/clamav.sh
+++ QPKGs/ClamAV/shared/clamav.sh
@@ -34,7 +34,7 @@
 #*
 readonly r_user_args_raw=$*
 readonly r_qpkg_name=ClamAV
-readonly r_service_script_version=250726
+readonly r_service_script_version=250822
 InitService(){
 qpkg_ini_file=undefined
 qpkg_backup_pathfile=undefined
