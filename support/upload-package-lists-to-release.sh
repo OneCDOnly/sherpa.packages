e50be5c873467ae825a0c0971dabbc8b19d55df5
OneCDOnly <one.cd.only@gmail.com>
2025-09-14 06:11:59 +1000

    test push to release file

---
+++ support/upload-package-lists-to-release.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+. $HOME/scripts/nas/sherpa/support/environment.sourced || exit
+
+release_tag=v${build_date}
+
+echo "release_tag: '$release_tag'"
+
+gh release upload "$release_tag" $qpkgs_root_path/*.packages.tar.gz
