2b22c5f8e4999cb81c11cb2d586813a9916573a2
OneCDOnly <one.cd.only@gmail.com>
2025-10-12 12:12:10 +1000

    add post-install service action and results to FileBrowser

--- QPKGs/FileBrowser/setup-new-config.sh
+++ QPKGs/FileBrowser/setup-new-config.sh
@@ -2,5 +2,5 @@
 
 ./repo-cache/filebrowser config init -d ./config/filebrowser.db
 ./repo-cache/filebrowser config set -p 8641 -a '0.0.0.0' -d ./config/filebrowser.db
-./repo-cache/filebrowser users add admin 'iwillchangethispassword' -d ./config/filebrowser.db			# Use this to add an 'admin' user with a password.
-./repo-cache/filebrowser config set --auth.method=noauth -d ./config/filebrowser.db						# Use this for LAN-only access with no-login required.
+./repo-cache/filebrowser users add admin 'iwillchangethispassword' -d ./config/filebrowser.db		# To add an 'admin' user with a password.
+./repo-cache/filebrowser config set --auth.method=noauth -d ./config/filebrowser.db					# For LAN-only access with no-login required.
