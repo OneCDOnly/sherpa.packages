2e61f8633eef0344fe193e30f2862d50c996974d
OneCDOnly <one.cd.only@gmail.com>
2025-10-29 16:51:19 +1000

    [update] built files

--- support/update-built.sh
+++ support/update-built.sh
@@ -2,4 +2,4 @@
 
 . $HOME/scripts/nas/sherpa/support/environment.sourced || exit
 
-git add "$qpkgs_root_path"/*.{cfg,md5,lib,sh} && git commit -m '[update] built files' && git push
+git add "$qpkgs_root_path"/*.{cfg,md5,lib,sh,whl} && git commit -m '[update] built files' && git push
