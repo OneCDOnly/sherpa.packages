fe2ab8032935e8015fd51a2bba6f14e4d9aae51b
OneCDOnly <one.cd.only@gmail.com>
2025-11-24 08:02:31 +1000

    update PyPI deps for WeeWX

--- QPKGs/WeeWX/shared/weewx.sh
+++ QPKGs/WeeWX/shared/weewx.sh
@@ -34,8 +34,9 @@
 #*
 readonly r_user_args_raw=$*
 readonly r_qpkg_name=WeeWX
-readonly r_service_script_version=251112
+readonly r_service_script_version=251124
 InitService(){
+allow_access_to_sys_packages=true
 can_restart_to_update=true
 daemon_pidfile_is_managed_by_app=true
 install_pip_deps=true
