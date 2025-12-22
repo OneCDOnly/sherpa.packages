0fa660693801c0f8d4cd9e64389c2133a1b589c3
OneCDOnly <one.cd.only@gmail.com>
2025-12-23 08:20:50 +1000

    [update] built files

--- QPKGs/pxehost/shared/pxehost.sh
+++ QPKGs/pxehost/shared/pxehost.sh
@@ -34,7 +34,7 @@
 #*
 readonly r_user_args_raw=$*
 readonly r_qpkg_name=pxehost
-readonly r_service_script_version=251220
+readonly r_service_script_version=251221
 InitService(){
 check_network_before_start=true
 run_daemon_in_screen_session=true
