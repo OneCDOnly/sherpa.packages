6cb794f317685c6e73b8d24063b3f37bd41bc7b6
OneCDOnly <one.cd.only@gmail.com>
2026-09-07 08:23:53 +1000

    [update] built files

--- QPKGs/Bazarr/shared/bazarr.sh
+++ QPKGs/Bazarr/shared/bazarr.sh
@@ -36,7 +36,7 @@
 #*
 readonly r_user_args_raw=$*
 readonly r_qpkg_name=Bazarr
-readonly r_service_script_version=260906
+readonly r_service_script_version=260907
 InitService(){
 allow_access_to_sys_packages=true
 can_restart_to_update=true
