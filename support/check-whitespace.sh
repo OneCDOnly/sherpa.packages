03c8d84e4857e0223ee2a8395daaab57c3b8ecb2
OneCDOnly <one.cd.only@gmail.com>
2026-09-11 21:27:56 +1000

    update build tags

--- support/check-whitespace.sh
+++ support/check-whitespace.sh
@@ -13,8 +13,8 @@ a_a+=("$service_library_source_file")
 for i in "${!a_a[@]}"; do
 	echo -n "checking for unwanted whitespace '${a_a[i]}' ... "
 
-	b=$(grep -nP ' \t' "${a_a[i]}")												# check for space char followed by tab char (should never happen).
-	b+=$(grep -nF '    ' "${a_a[i]}" | grep -v 'dont-squeeze\|ignore-leader')	# check for 4 consecutive space chars.
+	b=$(grep -nP ' \t' "${a_a[i]}")										# check for space char followed by tab char (should never happen).
+	b+=$(grep -nF '    ' "${a_a[i]}" | grep -v 'directive:dont-squeeze\|directive:ignore-leader')	# check for 4 consecutive space chars.
 
 	if [[ -z $b ]]; then
 		ShowDone
