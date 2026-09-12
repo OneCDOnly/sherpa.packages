a2db8164de84cdd96cc0eb349b19ab38086e2e44
OneCDOnly <one.cd.only@gmail.com>
2026-09-12 11:19:00 +1000

    update: builder scripts commit messages

--- support/update-wheels.sh
+++ support/update-wheels.sh
@@ -2,4 +2,4 @@
 
 . $HOME/scripts/nas/sherpa/support/environment.sourced || exit
 
-git add "$qpkgs_root_path"/*.whl "$qpkgs_root_path"/*pypi-lists* && git commit -m '[update] PyPI wheels' && git push
+git add "$qpkgs_root_path"/*.whl "$qpkgs_root_path"/*pypi-lists* && git commit -m 'update: PyPI wheels' && git push
